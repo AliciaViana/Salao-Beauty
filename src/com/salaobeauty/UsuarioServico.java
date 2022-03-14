c3eac0d06a0bfaba8ea4a4526c9608fedbd995b5
AliciaViana <aliciasouza601@gmail.com>
2022-03-13 21:49:13 -0300

    inclusao validacao

--- src/com/salaobeauty/UsuarioServico.java
+++ src/com/salaobeauty/UsuarioServico.java
@@ -3,9 +3,31 @@ package com.salaobeauty;
 public class UsuarioServico {
 
 	public void salvaUsuario(Usuario usuario) {
-		System.out.println("o usuario " + usuario.getNome()
-		+ " foi salvo com sucesso ");
-
+		// null
+		// isEmpty()
+		if (this.validaUsuario(usuario) == true) {
+			System.out.println("o usuario " + usuario.getNome() + " foi salvo com sucesso ");
+		} else {
+			System.out.println("O usuário não foi salvo");
+		}
 
 	}
+
+	public boolean validaUsuario(Usuario usuario) {
+		if (usuario.getNome().isEmpty()) {
+			System.out.println("o campo nome nao pode ficar vazio");
+			return false;
+
+		} else if (usuario.getCpf().isEmpty()) {
+			System.out.println("o campo cpf nao pode ficar vazio");
+			return false;
+
+		} else if (usuario.getEmail().isEmpty()) {
+			System.out.println("o campo email nao pode ficar vazio");
+			return false;
+
+		} else
+			return true;
+	}
+
 }
